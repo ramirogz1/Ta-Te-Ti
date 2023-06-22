55b70ae9cd5cd2bb91976bf0545c935d17fee0e4
ramirogz1 <ramirogz101@gmail.com>
2023-06-22 19:03:52 -0300

    proyect terminado

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import TicTacToe from './TaTeTiComp'
+
+function App() {
+  return(
+    <div className='App'>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
